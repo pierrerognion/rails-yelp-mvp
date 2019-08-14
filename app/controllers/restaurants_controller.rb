2fcc9c2e6ceae57981afc662123deb9e5470de25
Pierre Rognion <pierrerognion@gmail.com>
2019-08-14 13:01:06 +0200

    progress

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,22 +1,47 @@
 class RestaurantsController < ApplicationController
-  def index         # GET /restaurants
+  def index
+    @restaurants = Restaurant.all
   end
 
-  def show          # GET /restaurants/:id
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
-  def new           # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
   end
 
-  def create        # POST /restaurants
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
-  def edit          # GET /restaurants/:id/edit
+  def edit
+    @restaurant = Restaurant.find(params[:id])
   end
 
-  def update        # PATCH /restaurants/:id
+  def update
+    @restaurants = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
   end
 
-  def destroy       # DELETE /restaurants/:id
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :category, :address)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,41 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.save
+    redirect_to reviews_path
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @reviews = Review.find(params[:id])
+    @reviews.update(review_params)
+    redirect_to reviews_path
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:name, :category)
+  end
 end
