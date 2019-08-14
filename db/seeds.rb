da7662c61e48d035213a19b657a276faad583964
Pierre Rognion <pierrerognion@gmail.com>
2019-08-14 12:01:35 +0200

    progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating 5 restaurants...'
+restaurants_attributes = [
+  {
+    name:         'PNY',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'italian',
+  },
+  {
+    name:         'Mamma Roma',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+  },
+  {
+    name:         'Esprit Chai',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'french',
+  },
+  {
+    name:         'La Brigade',
+    address:      '56A2 Shoreditch High St, London E1 6PQ',
+    category:  'french',
+  },
+  {
+    name:         'Bocamexa',
+    address:      '56A6 Shoreditch High St, London E1 6PQ',
+    category:  'french',
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
