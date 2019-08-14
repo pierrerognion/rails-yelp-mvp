bc6a32102ac1b6e898ddc0d178d6417089cbb602
Pierre Rognion <pierrerognion@gmail.com>
2019-08-14 14:47:32 +0200

    done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,13 +8,18 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
-    @review.save
-    redirect_to reviews_path
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -36,6 +41,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:name, :category)
+    params.require(:review).permit(:content, :rating, :restaurant_id)
   end
 end
